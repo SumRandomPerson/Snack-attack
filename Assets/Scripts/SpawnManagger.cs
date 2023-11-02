4ad99a2427648e9e508d51f82a1d09594150f914
SumRandomPerson <113185567+SumRandomPerson@users.noreply.github.com>
2023-11-02 13:19:44 -0400

    changes

---
+++ Assets/Scripts/SpawnManagger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagger : MonoBehaviour
+{
+public GameObject[] itemPrefabs;
+    float SpawnRangeX = 9;
+    float spawnPosZ = 9;
+    public float startingSpawn = 5; 
+    public float spawnTiming = 1.5f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomItem", startingSpawn, spawnTiming);
+    }
+
+    void SpawnRandomItem()
+
+    {
+        
+        Vector3 spawnPos = new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 10, Random.Range(-spawnPosZ, spawnPosZ)); 
+            
+            int itemIndex = Random.Range(0, itemPrefabs.Length); 
+            
+            Instantiate(itemPrefabs[itemIndex], spawnPos, itemPrefabs[itemIndex].transform.rotation);
+
+    }}
